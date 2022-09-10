1bbc903f44835b7d30bd65e87fe40ee0df3ee693
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-09-10 20:41:24 +0300

    feat: add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
+	let [search, setSearch] = useState("");
 	let [fetchedData, setFetchedData] = useState([]);
 	let [pageNumber, setPageNumber] = useState(1);
-	let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+	let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 	let { info, results } = fetchedData;
 
 	useEffect(() => {
@@ -24,7 +26,7 @@ function App() {
 			<h1 className="text-center ubuntu my-4">
 				Rick & Morty <span className="text-primary">WiKi</span>
 			</h1>
-
+			<Search setSearch={setSearch} setPageNumber={setPageNumber} />
 			<div className="container">
 				<div className="row">
 					<div className="col-3">
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Search.module.scss";
+
+const Search = ({ setSearch, setPageNumber }) => {
+	let searchHandler = (e) => {
+		setSearch();
+	};
+	return (
+		<form className="d-flex justify-content-center gap-4 mb-5">
+			<input
+				className={styles.input}
+				type="text"
+				placeholder="Start Typing Character Name..."
+				onChange={(e) => {
+					setPageNumber(1);
+					setSearch(e.target.value);
+				}}
+			/>
+			{/* <button
+				className={`${styles.btn} btn btn-primary fs-5`}
+				onClick={(e) => e.preventDefault()}
+			>
+				Search
+			</button> */}
+		</form>
+	);
+};
+
+export default Search;
