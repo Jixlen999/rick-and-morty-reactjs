d117bcb3d117e79bb892d9e2cac1a5c10a498151
Victoria Ivanyutenko <viktoriyaivanyutenko@yandex.ru>
2022-09-13 19:36:05 +0300

    fix: try fixing paths

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,25 @@ function App() {
 				<NavBar />
 			</div>
 			<Routes>
+				<Route path="rick-and-morty-reactjs/" element={<Home />} />
 				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/"
-					element={<Home />}
-				/>
-				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/:id"
+					path="rick-and-morty-reactjs/:id"
 					element={<CardDetails />}
 				/>
 				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/episodes"
+					path="rick-and-morty-reactjs/episodes"
 					element={<Episodes />}
 				/>
 				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/episodes/:id"
+					path="rick-and-morty-reactjs/episodes/:id"
 					element={<CardDetails />}
 				/>
 				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/locations"
+					path="rick-and-morty-reactjs/locations"
 					element={<Locations />}
 				/>
 				<Route
-					path="https://jixlen999.github.io/rick-and-morty-reactjs/locations/:id"
+					path="rick-and-morty-reactjs/locations/:id"
 					element={<CardDetails />}
 				/>
 			</Routes>
